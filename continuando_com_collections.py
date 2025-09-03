37491914b4bf3f605cfc9afe77d7703de0e285d0
pedro-henrique-silva17 <164204915+pedro-henrique-silva17@users.noreply.github.com>
2025-09-03 11:00:53 -0300

    Update continuando_com_collections.py

--- continuando_com_collections.py
+++ continuando_com_collections.py
@@ -50,3 +50,27 @@ fez_ds_mas_nao_fez_ml = usuarios_data_science - usuarios_machine_learning
 
 usuarios_data_science ^ usuarios_machine_learning
 
+usuarios = {1,5,76,34,52,13,17}
+len(usuarios)
+
+usuarios.add(13)
+len(usuarios)
+
+usuarios.add(765)
+len(usuarios)
+
+usuarios
+
+usuarios = frozenset(usuarios)
+usuarios
+
+type(usuarios)
+
+usuarios.add(134)
+
+meu_texto = "Bem vindo meu nome é Guilherme eu gosto muito de nomes e tenho o meu cachorro e gosto muito de cachorro"
+meu_texto.split()
+
+set(meu_texto.split())
+
+
